53a79f1c6604ceaa9449b6cad8a6a91eca6e52dc
Max071298 <102859892+Max071298@users.noreply.github.com>
2023-09-20 20:56:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let config = {};
+  let OPEN_BRACKETS = [];
+  let CLOSE_BRACKETS = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    config[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    OPEN_BRACKETS.push(bracketsConfig[i][0]);
+    CLOSE_BRACKETS.push(bracketsConfig[i][1]);
+  }
+
+  let resultArray = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      OPEN_BRACKETS.findIndex((item) => item === str[i]) !== -1 &&
+      CLOSE_BRACKETS.findIndex((item) => item === str[i]) !== -1
+    ) {
+      if (resultArray.at(-1) === str[i]) {
+        resultArray.pop();
+      } else resultArray.push(str[i]);
+    } else if (OPEN_BRACKETS.findIndex((item) => item === str[i]) !== -1) {
+      resultArray.push(str[i]);
+    } else if (resultArray.at(-1) === config[str[i]]) {
+      resultArray.pop();
+    } else return false;
+  }
+
+  return resultArray.length === 0;
+};
